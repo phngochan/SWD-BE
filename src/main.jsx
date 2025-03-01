b79d587256a4cb49db11d2c1792446d7215e735a
htqnhat280203 <htqnhat280203@gmail.com>
2025-03-01 14:37:33 +0700

    update Register call api

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,15 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import Login from './Login.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Login />
-//   </StrictMode>,
-// )
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import AppRoute from './routes/AppRoutes.jsx';
+import AppRoute from "./routes/AppRoutes.jsx";
+import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <AppRoute />
-  </BrowserRouter>,
-  document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
+  </React.StrictMode>
 );
