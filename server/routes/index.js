6f697b0d35306e185cc712d2c78d8ec05e86ae53
Duongtkse182382 <duongtkse182382@fpt.edu.vn>
2025-03-05 13:13:26 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
-// const bookingRequestRoutes = require('./bookingRequestRoutes');
-// const feedbackRoutes = require('./feedbackRoutes');
-// const serviceRoutes = require('./serviceRoutes');
+const customerRoutes = require('./customerRoutes');
+const staffRoutes = require('./staffRoutes');
+const managerRoutes = require('./managerRoutes');
+const consultantRoutes = require('./consultantRoutes');
+const bookingRequestRoutes = require('./bookingRequestRoutes');
+const feedbackRoutes = require('./feedbackRoutes');
+const serviceRoutes = require('./serviceRoutes');
 const quizResultRoutes = require('./quizResultRoutes');
-// const paymentRoutes = require('./paymentRoutes');
-// const blogRoutes = require('./blogRoutes');
+const paymentRoutes = require('./paymentRoutes');
+const blogRoutes = require('./blogRoutes');
 const questionRoutes = require('./questionRoutes');
-const managerRoutes = require("./managerRoutes");
-// const calendarRoutes = require("./calendarRoutes");
-// const consultantRoutes = require("./consultantRoutes");
+const calendarRoutes = require('./calendarRoutes');
+
 const router = express.Router();
 
+router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/customers', customerRoutes);
+router.use('/staff', staffRoutes);
 router.use('/managers', managerRoutes);
-router.use('/questions', questionRoutes);
+router.use('/consultants', consultantRoutes);
+router.use('/booking-requests', bookingRequestRoutes);
+router.use('/feedbacks', feedbackRoutes);
+router.use('/services', serviceRoutes);
 router.use('/quiz-results', quizResultRoutes);
-// router.use('/consultants', consultantRoutes);
+router.use('/payments', paymentRoutes);
+router.use('/blogs', blogRoutes);
+router.use('/questions', questionRoutes);
+router.use('/calendars', calendarRoutes);
 
 module.exports = router;
-
