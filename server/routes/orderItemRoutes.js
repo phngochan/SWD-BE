da8ef7d5cefa8a92f69d3b2b53d3e53201f19a99
duccuongdeptrai <CuongNDHE173301@fpt.edu.vn>
2025-03-15 13:13:08 +0700

    tạo đơn hàng
    tạo đơn hàng

---
+++ server/routes/orderItemRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getOrderItems, addOrderItem, deleteOrderItem } = require("../controllers/OrderItemController");
+const { authenticate, authorize } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.get("/:orderID", authenticate, getOrderItems);
+router.post("/", authenticate, authorize(["Customer"]), addOrderItem);
+router.delete("/:id", authenticate, authorize(["Customer"]), deleteOrderItem);
+
+module.exports = router;
